a4975fac848a7135c338310dd58868a74a06c8d4
Stefan <stefan131@outlook.com>
2018-03-28 09:34:24 +0200

    Created Price class which adds static prices for movies Added double Price to Ticket constructor

---
+++ app/src/main/java/nl/seventa/estetika/domain/Price.java
@@ -0,0 +1,16 @@
+package nl.seventa.estetika.domain;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+/**
+ * Created by Stefan on 28-3-2018.
+ */
+
+public class Price {
+    public static final double REGULAR = 10;
+    public static final double YOUTH = 5;
+    public static final double DISCOUNT = 7.5;
+}
+
